462978656cf4822f1bb31ee53ec7bd0e06bd3a7e
vani172 <vani172@gmail.com>
2016-12-15 00:53:18 +0200

    moved menu to separate view and added log off functionality

---
+++ SocialBlade/src/SocialBlade/Models/AccountViewModels/ProfileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialBlade.Models.AccountViewModels
+{
+    public class ProfileViewModel
+    {
+        public string FullName { get; set; }
+        public string ProfileImageUrl { get; set; }
+        public string FollowersCount { get; set; }
+    }
+}
---
+++ SocialBlade/src/SocialBlade/ViewComponents/MenuComponent.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SocialBlade.Models;
+using SocialBlade.Models.AccountViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialBlade.ViewComponents
+{
+    [ViewComponent(Name = "MenuComponent")]
+    public class MenuComponent:ViewComponent
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+        public MenuComponent(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var model = new ProfileViewModel();
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                model.FullName = user.FirstName + " " + user.LastName;
+                model.ProfileImageUrl = "";
+                model.FollowersCount = "14M followers";
+                return View(model);
+            }
+            return null;
+        }
+    }
+}
