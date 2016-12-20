890782a212c5afe5cb28ee8be24786cfbe60f808
BobbyRackov <bobisvetoslavovrachkov@hotmail.com>
2016-12-20 18:21:55 +0200

    for test

--- SocialBlade/src/SocialBlade/wwwroot/js/followButtonsLogic.js
+++ SocialBlade/src/SocialBlade/wwwroot/js/followButtonsLogic.js
@@ -10,7 +10,6 @@ $('.follow-btn').click(function () {
             success: function (data) {
                 if (data.Status === '200') {
                     clearPosts(id);
-                    console.log("called");
                 }
             }
         });
@@ -20,7 +19,6 @@ function clearPosts(authorId) {
     let body = $("body");
     let posts = body.find(".post");
     let smallPosts = body.find(".post-small");
-    console.log(authorId);
     for (let i = 0; i < posts.size() ; i++) {
         let post = posts[i];
         let id = $(post).find(".author-id").val();
@@ -40,43 +38,3 @@ function clearPosts(authorId) {
         }
     };
 }
-
-function toggleFollowButton() {
-    if ($('.follow-btn').hasClass('following')) {
-        $('.follow-btn').removeClass('following');
-    }
-    else {
-        $('.follow-btn').addClass('following');
-    }
-}
-
-$('.follow-btn').mouseenter(function () {
-    if ($(this).hasClass('following')) {
-        $(this).find('.already').hide();
-        $(this).find('.unfollow').show();
-        $(this).css("background", "red");
-        $(this).css("color", "#eee");
-    }
-});
-
-$('.follow-btn').mouseleave(function () {
-    if ($(this).hasClass('following')) {
-        $(this).find('.unfollow').hide();
-        $(this).find('.already').show();
-        $(this).css("background", "green");
-    }
-});
-/*let me = $(this);
-    $.ajax(
-        {
-            type: "POST",
-            url: "/Account/ToggleFollow",
-            data: { 'userId': $('#ID').val() },
-            success: function (data) {
-                if(data.Status === '200')
-                {
-                    toggleFollowButton();
-                    updateFollowersCount(data.FollowersCount);
-                }
-            }
-        });*/
